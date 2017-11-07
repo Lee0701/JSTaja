eeadc2fd6b05bdcadb3df591319285ecc9e2aaee
Lee0701 <hgl1002@naver.com>
2017-11-07 13:52:57 +0900

    Renamed index.html to text.html

--- taja.js
+++ taja.js
@@ -34,9 +34,12 @@ const tajaLoad = function() {
   input.addEventListener("keydown", textInput);
   input.addEventListener("keyup", textInput);
   
-  pageText = composingText = '';
-  
   focusOut();
+  
+}
+
+const textLoad = function() {
+  pageText = composingText = '';
   
   practiceText = "동해물과 백두산이 마르고 닳도록\n하느님이 보우하사 우리나라 만세\n무궁화 삼천리 화려강산\n대한사람 대한으로 길이 보전하세";
   
